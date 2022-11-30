6c73c2c31046adaa3323c89e4f2c0ac06143b658
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-30 11:19:24 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include "primaLibreria.h"
 
+int potenza(int a,int b){
+    int risultato=1;
+    for(int i=0;i<b;i++){
+        risultato = risultato*a;
+    }
+    return risultato;
+}
+
+int potenza(int a){
+    return a*a;
+}
+
+int fattoriale(int a){
+    int result = 1;
+    while(a>1){
+        result = result*a;
+        a--;
+    }
+    return result;
+}
