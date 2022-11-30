fb2e8ae4cab89560451d5b0d00545e812ecd640c
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-30 11:50:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
+using namespace std;
+
+int main() {
+    int a = 0;
+    cin >> a;
+    if(numero_primo(a,a)){
+        cout << "numero primo";
+    }else{
+        cout << "numero non primo";
+    }
   return 0;
 }
