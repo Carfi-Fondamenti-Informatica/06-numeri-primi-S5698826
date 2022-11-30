9dbd9ef0b731687f37ad7c24612addd78974bf30
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-11-30 11:21:01 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+#ifndef PRIMALIBRERIA_PRIMALIBRERIA_H
+#define PRIMALIBRERIA_PRIMALIBRERIA_H
 
+int potenza(int);
+
+int potenza(int, int);
+
+int fattoriale(int);
+
+#endif //PRIMALIBRERIA_PRIMALIBRERIA_H
