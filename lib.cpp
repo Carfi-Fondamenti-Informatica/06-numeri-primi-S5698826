f77e1bf369e90d7a1383b8889b7cb32cb2657345
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-01 10:06:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,11 +3,11 @@
 //
 #include "primaLibreria.h"
 
-bool numeroPrimo(int a){
+bool numeroPrimo(int x){
     bool primo = true;
-    for (int i=2; i<a; i++){
-        int risultato=a%i;
-        if (risultato==0 and i<a){
+    for (int i=2; i<x; i++){
+        int risultato=x%i;
+        if (risultato==0 and i<x){
             primo=false;
             break;
         }
