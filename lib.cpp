e3b6c4a0f893e382dac87c4f917e999cbc438e2a
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-01 09:47:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,22 +1,13 @@
 #include "primaLibreria.h"
 
-int potenza(int a,int b){
-    int risultato=1;
-    for(int i=0;i<b;i++){
-        risultato = risultato*a;
+bool numeroPrimo(int a){
+    bool primo = true;
+    for (int i=2; i<a; i++){
+        int risultato=a%i;
+        if (risultato==0 and i<a){
+            primo=false;
+            break;
+        }
     }
-    return risultato;
-}
-
-int potenza(int a){
-    return a*a;
-}
-
-int fattoriale(int a){
-    int result = 1;
-    while(a>1){
-        result = result*a;
-        a--;
-    }
-    return result;
+    return primo;
 }
